bd1552d63f32545df2b8ae0da7530a88773bb493
Mag <magda.jarozeski.pb@compasso.com.br>
2022-01-11 16:39:19 -0300

    Questão 05

---
+++ Bot-Agil.js
@@ -0,0 +1,8 @@
+const pergunta = (numero) => {
+  if (numero === 1) return "Planning reunião realizada antes do início de uma sprint para planejamento"
+	if (numero === 2) return "Sprint é uma forma de organizar o tempo para realização de determiandas atividades, geralmente tem duração de duas semanas."
+	   if(numero === 3) return "Retro acontece após o termino da sprint, antes de começar um novo planning."
+	return "Encerrar"
+
+}
+
